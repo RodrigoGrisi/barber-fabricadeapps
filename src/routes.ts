14641d4fa5ce5ef4324954896ffca11ec20740a2
rodrigogrisi <Rodricosta1995@gmail.com>
2024-03-26 17:50:20 -0300

    Feat: User, section, and user update features ok

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,15 @@ import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
 import { DetailUserController } from "./controllers/user/DetailUserController";
+import { isAuthenticated } from "./middlewares/isAuthenticated";
+import { UpdateUserController } from "./controllers/user/UpdateUserController";
 
 const router = Router();
 
 //ROTAS USER //
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
-router.get("/me", new DetailUserController().handle);
+router.get("/me", isAuthenticated, new DetailUserController().handle);
+router.put("/users", isAuthenticated, new UpdateUserController().handle);
 
 export { router };
