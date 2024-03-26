949907ef72999971e6ed6641ec859465e3a73400
rodrigogrisi <Rodricosta1995@gmail.com>
2024-03-26 11:09:56 -0300

    Feat: Starting middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,13 @@
 import { Router, Request, Response } from "express";
 import { CreateUserController } from "./controllers/user/CreateUserController";
 import { AuthUserController } from "./controllers/user/AuthUserController";
+import { DetailUserController } from "./controllers/user/DetailUserController";
 
 const router = Router();
-
-// router.get("/teste", (req: Request, res: Response) => {
-//   // throw new Error("TESTE AQUI")
-//   return res.json({ ok: false });
-// });
 
 //ROTAS USER //
 router.post("/users", new CreateUserController().handle);
 router.post("/session", new AuthUserController().handle);
+router.get("/me", new DetailUserController().handle);
 
 export { router };
