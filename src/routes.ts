2baddf9252e6ac03d8f8ec1b6456bfb71eb8f9f8
rodrigogrisi <Rodricosta1995@gmail.com>
2024-03-28 00:01:40 -0300

    Feat: List of curts ok

--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { UpdateUserController } from "./controllers/user/UpdateUserController";
 import { CreateHaircutController } from "./controllers/haircut/CreateHaircutController";
 import { CreateUserService } from "./services/user/CreateUserService";
+import { ListHaircutController } from "./controllers/haircut/ListHaircutController";
 
 const router = Router();
 
@@ -17,4 +18,6 @@ router.put("/users", isAuthenticated, new UpdateUserController().handle);
 
 // --- ROTAS HAIRCUT
 router.post("/haircut", isAuthenticated, new CreateHaircutController().handle);
+router.get("/haircuts", isAuthenticated, new ListHaircutController().handle);
+
 export { router };
--- src/services/haircut/ListaHaircutService.ts
+++ src/services/haircut/ListaHaircutService.ts
@@ -1,17 +1,28 @@
 import prismaClient from "../../prisma";
+import { Haircut } from ".prisma/client";
 
 interface HaircutRequest {
   user_id: string;
-  status: boolean | boolean;
+  status: boolean | string;
 }
 
 class ListHaircutService {
-  async execute({ user_id, status }: HaircutRequest) {
-    const haircut = prismaClient.haircut.findMany({
+  async execute({ user_id, status }: HaircutRequest): Promise<Haircut[]> {
+    // Utilize o findMany para buscar todos os cortes de cabelo com base nos filtros
+    const haircuts = await prismaClient.haircut.findMany({
       where: {
         user_id: user_id,
-        status: status,
+        status: status === "true" ? true : false,
       },
     });
+
+    // Verifique se há cortes de cabelo retornados
+    if (!haircuts) {
+      throw new Error("Cuts not found");
+    }
+
+    return haircuts;
   }
 }
+
+export { ListHaircutService };
